21925a02ea7fe10bb5b5102c9d92046bcd013c62
Anastasia <matr-nastya@yandex.ru>
2021-05-25 20:37:22 +0300

    Modified test

--- src/test/java/ru/netology/test/postmanEcho.java
+++ src/test/java/ru/netology/test/postmanEcho.java
@@ -10,16 +10,18 @@ public class postmanEcho {
 
     @Test
     public void postmanEchoTest(){
-        given()
-                .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+            // Given - When - Then
+// Предусловия
+            given()
+                    .baseUri("https://postman-echo.com")
+                    .body("User id") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
-                .when()
-                .post("/post")
+                    .when()
+                    .post("/post")
 // Проверки
-                .then()
-                .statusCode(200)
-                .body("data", equalTo("User id"))
-        ;
-    }
+                    .then()
+                    .statusCode(200)
+                    .body("data", equalTo("User"))
+            ;
+        }
 }
